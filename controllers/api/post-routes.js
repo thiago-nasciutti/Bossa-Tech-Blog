88a1835783d6a734acba84d8f9de5f48b04fb51c
thiago <nasciutti.thiago@gmail.com>
2022-10-24 09:56:25 -0400

    homepage-view

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_url',
+      'post_text',
       'title',
       'created_at'
     ],
@@ -40,7 +40,7 @@ router.get('/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'post_text',
       'title',
       'created_at'
     ],
